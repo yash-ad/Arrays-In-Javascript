fd3fce66b116ecc6da70a3e8eaf7da88340cf2be
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-03-24 09:03:44 +0530

    Array learning

---
+++ script.js
@@ -0,0 +1,18 @@
+//Array is a collection of data.
+//[square bracket] is called literal.
+//It has become a common practice to declare arrays using const and always declare with const
+//In the square bracket we called elements likewise it could be string , number or boolean etc.
+
+const myArray = ["Y","A","S","H","R","A","J",true,false,31,1998]; //Array in javascript can be of mixed data types 
+console.log(myArray);
+console.log(myArray[6]); //You can access elements of an array using indices.0,1,2 etc.
+console.log(myArray.length); //Array properties:- You can find the length of an element using length property
+console.log();
+console.log();
+
+
+
+const fruits = ["Apple","Mango","Banana","Strawberry","Pineapple"];
+console.log(fruits);
+console.log(fruits[3]);
+
