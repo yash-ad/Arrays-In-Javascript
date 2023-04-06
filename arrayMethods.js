16b939678ee904f9b46e7acf6e5275810f1f215e
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-04-06 15:28:36 +0530

    new file added arrayMethods.js

---
+++ arrayMethods.js
@@ -0,0 +1,9 @@
+//Array Methods
+
+//toString():- Converts an array to a string of comma seperated values;
+//typeOf string;
+
+const nums = [1,2,3,4,5,6];
+
+let resultOne = nums.toString();
+console.log(resultOne);
