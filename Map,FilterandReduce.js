6a186fe322bf5f9b26f1d000aae3460c445875b0
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-04-19 12:23:26 +0530

    Filter method

--- Map,FilterandReduce.js
+++ Map,FilterandReduce.js
@@ -1,5 +1,7 @@
 //Map, Filter & Reduce in JavaScript | JavaScript :-
+  
 
+//001.Map() method:-
 
 //1.Addition all the values of an array:-
 let arr = [ 24 , 48 , 56 ];
@@ -35,3 +37,19 @@ console.log(value);
 
 })
 console.log(arr_Three); //  [0, 1, 2, 3, 4]
+
+
+//002.Filter() Method :-
+
+//4. Lets findout the numbers that are lesser than 10 :-
+let numbers = [ 99 , 70 , 40 , 20 , 30  , 5 , 4 , 50];
+
+let arr_Four = numbers.filter((value)=>{
+
+console.log(value);
+return value < 10;
+ 
+
+})
+
+console.log(arr_Four); //  [5, 4] There are only two numbers found that are lesser than 10;
