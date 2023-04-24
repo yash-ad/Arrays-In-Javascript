16fcc876d69d60203f975619766a6617770e718f
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-04-24 11:22:36 +0530

    practice set on arrays question 2

--- Practice_Set on Arrays.js
+++ Practice_Set on Arrays.js
@@ -4,10 +4,41 @@
 
 const numbers = [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 ];
 
-let user_Input = prompt("Enter a number :");
+let user_Input = prompt("Enter a number :");// 55 // 31
 
 user_Input = Number.parseInt(user_Input) // why do we used parseInt because user_Input its a string and parseInt is used for converting string to an integer or a number.
+//Using push() method
+ numbers.push(user_Input); //Add the element to the end of an array
 
-numbers.push(user_Input);
+ console.log(numbers); // [1, 2, 3, 4, 5, 6, 7, 8, 55] , [1, 2, 3, 4, 5, 6, 7, 8, 31] 
 
-console.log(numbers);
+
+//Using unshift() method
+numbers.unshift(user_Input) //Add the element to the beginning of an array
+console.log(numbers); // [55, 1, 2, 3, 4, 5, 6, 7, 8]
+
+
+
+
+//2.Keep adding numbers to the array from question 1 using while or do while loop with an array until 0 is added to the array:-
+
+const theNumbers = [11 , 12 , 13 , 14 , 15 , 16 ];
+let a;
+do {let userInput = prompt("Enter your number here : ");
+
+    userInput = Number.parseInt(user_Input);
+
+theNumbers.push(userInput);
+}
+
+while(a != 0);
+
+console.log(theNumbers);
+
+
+
+
+
+
+
+
